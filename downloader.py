801742584866a2b732fb0758a541702b89541067
Samolaev <Gari5782014@gmail.com>
2026-01-16 20:33:47 +0300

    Убрал yt-dlp

--- downloader.py
+++ downloader.py
@@ -1,5 +1,4 @@
 import requests
-import yt_dlp
 
 def download_video(url):
     # Попытка через Cobalt
@@ -12,22 +11,6 @@ def download_video(url):
             video_response = requests.get(download_url)
             if video_response.status_code == 200:
                 return video_response.content, None
-    except:
-        pass
-
-    # Если Cobalt не сработал, yt-dlp
-    try:
-        ydl_opts = {
-            'format': 'best[height<=720]',  # Короткие видео, ограничиваем качество
-        }
-        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-            info = ydl.extract_info(url, download=False)
-            if 'formats' in info:
-                format = info['formats'][-1]  # Лучший формат
-                video_url = format['url']
-                video_response = requests.get(video_url)
-                if video_response.status_code == 200:
-                    return video_response.content, None
     except:
         pass
 
